7cf9b6c63cc9ff6bd005a6ec95f7dc4085e418d0
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-06-14 16:35:04 -0300

    Projeto Dice melhorado
    Criação de dados com imagens

--- app/src/main/java/com/curso/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/curso/android/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.curso.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 // Esta activity permite ao usuário rolar um dado e visualizar o resultado na tela.
@@ -14,17 +15,32 @@ class MainActivity : AppCompatActivity() {
         // Apontando para a referência de botão button e chamando a função rollDice ao clicar
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        // O dado é rolado quando abre o app
+        rollDice()
     }
 
-    // Função criando um objeto Dice, lançando o dado e mostrando em textView
+    // Função criando um objeto Dice, lançando o dado e mostrando em imageView
     private fun rollDice() {
-        // Crie um novo objeto Dice com 6 lados e role-o
+        // Cria um novo objeto Dice com 6 lados e role-o
         val dice = Dice(6)
         val diceRoll: Int = dice.roll()
 
-        // Atualize a tela com o lançamento de dados
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
@@ -32,4 +48,5 @@ class MainActivity : AppCompatActivity() {
 class Dice(private val numSides: Int) {
 
     fun roll(): Int = (1..numSides).random()
+
 }
