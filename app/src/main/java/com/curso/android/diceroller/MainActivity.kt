a01c78a0b1c9c65669ef167d224c6a01996de075
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2022-06-15 00:59:13 -0300

    Criação teste unitário

--- app/src/main/java/com/curso/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/curso/android/diceroller/MainActivity.kt
@@ -39,7 +39,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         diceImage.setImageResource(drawableResource)
-
         diceImage.contentDescription = diceRoll.toString()
     }
 }
